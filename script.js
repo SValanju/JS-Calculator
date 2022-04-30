69063ccc0fcd8cab780070487182030161b3c952
SValanju <67521500+SValanju@users.noreply.github.com>
2022-04-30 22:49:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,18 @@ function display(value){
     document.getElementById("result").value += value;
 }
 
+function Backspace(){
+    var input = document.getElementById("result");
+    var ss = input.selectionStart;
+    var se = input.selectionEnd;
+    var ln = input.value.length;
+
+    input.value = input.value.substring(0, ss-1) + input.value.substring(se, ln);
+    input.focus();
+    input.selectionStart = ss-1;
+    input.selectionEnd = ss-1;
+}
+
 function Calculate(){
     var a = document.getElementById("result").value;
     var b = eval(a);
